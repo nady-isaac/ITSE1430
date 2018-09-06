417f06e7a45edbbca64403b0261eb5cf80b63544
nady-isaac.lufuluabo <nady-isaac.lufuluabo@my.tccd.edu>
2018-09-05 19:10:42 -0500

    write

--- Classwork/program 1/program 1/Program.cs
+++ Classwork/program 1/program 1/Program.cs
@@ -4,21 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace program_1
+namespace Section1
 {
     class Program
     {
         static void Main( string[] args )
         {
-            //DisplayMenu();
-            PlayWithStrings();
+            bool notQuit;
+            do
+            {
+                notQuit = DisplayMenu();
+            } while (notQuit);
+
+            //PlayWithStrings();
         }
 
         private static void PlayWithStrings()
         {
             string hoursString = "10A";
 
-            //Form string
+            //From string
             //int hours = Int32.Parse(hoursString);
             //int hours;
             //bool result = Int32.TryParse(hoursString, out hours);
@@ -30,7 +35,7 @@ namespace program_1
             //457.ToString();
             //Console.ReadLine().ToString();
 
-            string message = "Hello\tworod";
+            string message = "Hello\tworld";
             string filePath = "C:\\Temp\\Test";
 
             //Verbatim strings
@@ -40,46 +45,171 @@ namespace program_1
             string firstName = "Bob";
             string lastName = "Smith";
             string name = firstName + " " + lastName;
+
+            //Strings are immutable - this produces a new string
+            //name = "Hello " + name;
+            Console.WriteLine("Hello " + name);  // Approach 1
+            Console.WriteLine("Hello {0} {1}", firstName, lastName); //Approach 2
+            string str = String.Format("Hello {0} {1}", firstName, lastName); //Approach 3
+            Console.WriteLine(str);
+
+            //Approach 4
+            Console.WriteLine($"Hello {firstName} {lastName}");
+
+            //Null vs empty
+            string missing = null;
+            string empty = "";
+            string empty2 = String.Empty;
+
+            //Checking for empty strings
+            //if (firstName.Length == 0)
+            //if (firstName != null && firstName != "")
+            if (!String.IsNullOrEmpty(firstName))
+                Console.WriteLine(firstName);
+
+            //Other stuff
+            string upperName = firstName.ToUpper();
+            string lowerName = firstName.ToLower();
+
+            //Comparison
+            bool areEqual = firstName == lastName;
+            areEqual = firstName.ToLower() == lastName.ToLower();
+            areEqual = String.Compare(firstName, lastName, true) == 0;
+
+            bool startsWithA = firstName.StartsWith("A");
+            bool endsWithA = firstName.EndsWith("A");
+            bool hasA = firstName.IndexOf("A") >= 0;
+            string subset = firstName.Substring(4);
+
+            //Clean up
+            string cleanMe = firstName.Trim(); //TrimStart, TrimEnd
+            string makeLonger = firstName.PadLeft(20); //PadRight
         }
 
-        private static void DisplayMenu()
+        private static void PlayWithArrays()
         {
-            Console.WriteLine("A)add Movie");
-            Console.WriteLine("E)dit Movie");
-            Console.WriteLine("D)elete Movie");
-            Console.WriteLine("V)iew Movie");
-            Console.WriteLine("Q)uit");
+            int count = ReadInt32("How many names? ", 1);
 
-            string input = Console.ReadLine();
-            switch (input[0])
+            string[] names = new string[count];
+            for (int index = 0; index < count; ++index)
             {
-                case 'A': AddMovie(); break;
-                case 'E': EditMovie(); break;
-                case 'D': DeleteMovie(); break;
-                case 'V': ViewMovie(); break;
-                case 'Q': break;
+                Console.WriteLine("Name? ");
+                names[index] = Console.ReadLine();
+            };
 
-                default: Console.WriteLine("Please enter a valid."); break;
+            foreach (string name in names)
+            //for (int index = 0; index < names.Length; ++index)
+            {
+                //readonly - not allowed
+                //name = "";
+                string str = name;
+                str = "";
+                //Console.WriteLine(names[index]);
+                Console.WriteLine(name);
+            };
+        }
+
+        private static bool DisplayMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("A)dd Movie");
+                Console.WriteLine("E)dit Movie");
+                Console.WriteLine("D)elete Movie");
+                Console.WriteLine("V)iew Movies");
+                Console.WriteLine("Q)uit");
+
+                string input = Console.ReadLine();
+                switch (input[0])
+                {
+                    case 'a':
+                    case 'A':
+                    AddMovie();
+                    return true;
+
+                    case 'e':
+                    case 'E':
+                    EditMovie();
+                    return true;
+
+                    case 'd':
+                    case 'D':
+                    DeleteMovie();
+                    return true;
+
+                    case 'v':
+                    case 'V':
+                    ViewMovies();
+                    return true;
+
+                    case 'q':
+                    case 'Q':
+                    return false;
+
+                    default:
+                    Console.WriteLine("Please enter a valid value.");
+                    break;
+                };
             };
         }
 
         private static void DeleteMovie()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("DeleteMovie");
         }
+
+        private static void ViewMovies()
+        {
+            Console.WriteLine("ViewMovies");
+        }
+
         private static void EditMovie()
         {
-            throw new NotImplementedException();
-
+            Console.WriteLine("EditMovie");
         }
+
         private static void AddMovie()
         {
-            throw new NotImplementedException();
+            name = ReadString("Enter a name: ", true);
+            description = ReadString("Enter a description:");
+            runLength = ReadInt32("Enter run length ( in minutes): ", 0);
         }
 
-        private static void ViewMovie()
+        private static int ReadInt32( string message, int minValue )
         {
-            throw new NotImplementedException();
+            while (true)
+            {
+                Console.WriteLine(message);
+                string input = Console.ReadLine();
+
+                if (Int32.TryParse(input, out int result))
+                {
+                    if (result >= minValue)
+                        return result;
+                };
+
+                Console.WriteLine($"You must enter an integer value >= {minValue}");
+            };
         }
+
+        private static string ReadString( string message )
+        {
+            return ReadString(message, false);
+        }
+        private static string ReadString( string message, bool required )
+        {
+            while (true)
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return input;
+
+            if (!String.IsNullOrEmpty(input) || !required)
+                return input;
+        }
+
+        // A movie
+        static string name;
+        static string description;
+        static int runLength;
+
     }
-}
